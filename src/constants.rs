d4ef50bce3b45295911c6a1eb54349993ef48e6c
ZapDark <jackaru@me.com>
2025-04-09 15:49:59 -0400

    tweak

--- src/constants.rs
+++ src/constants.rs
@@ -4,8 +4,8 @@ pub const INPUTS: i32 = 2;
 pub const BIAS: f32 = 1.0;
 pub const SCREEN_HEIGHT: i32 = 800;
 pub const SCREEN_WIDTH: i32 = 800;
-pub const DATA_POINTS: i32 = 200;
-pub const LEARNING_RATE: f32 = 0.005;
+pub const DATA_POINTS: i32 = 300;
+pub const LEARNING_RATE: f32 = 0.0005;
 //f(x)
 pub const SLOPE: f32 = 0.3;
 pub const SHIFT: f32 = 0.0;
